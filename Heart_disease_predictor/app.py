de4b1f57ea631233f558761907a09355c7254a1f
Arvind-17-svg <58883317+Arvind-17-svg@users.noreply.github.com>
2020-12-20 19:41:21 +0530

    Add files via upload

---
+++ Heart_disease_predictor/app.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 24 14:36:16 2020
+
+@author: LENOVO
+"""
+
+
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+import os
+model=pickle.load(open('svm.pkl','rb'))
+app=Flask(__name__)
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+
+
+
+@app.route('/predict',methods=['POST'])
+def home():
+    data1 = request.form['age']
+    data2 = request.form['sex']
+    data3 = request.form['cp']
+    data4 = request.form['trestbps']
+    data5 = request.form['chol']
+    data6 = request.form['fbs']
+    data7 = request.form['restecg']
+    data8 = request.form['thalach']
+    data9 = request.form['exang']
+    data10 = request.form['oldpeak']
+    data11 = request.form['slope']
+    data12 = request.form['ca']
+    data13 = request.form['thal']
+    arr = np.array([[data1, data2, data3, data4, data5, data6, data7, data8, data9, data10, data11, data12, data13]])
+    pred = model.predict(arr)
+    return render_template('about.html', data=pred)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
